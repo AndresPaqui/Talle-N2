ce315e690c6b353ce240d0ce7b3b556945c051fc
Steven Vaca <steven.vaca.galeas@udla.edu.ec>
2025-11-04 17:00:03 -0500

    Agregamos #indef

--- funciones.h
+++ funciones.h
@@ -1,3 +1,5 @@
+#ifndef FUNCIONES_H // Asegura que el contenido de funciones.h se procese unicamente una vez
+#define FUNCIONES_H  //Si no esta definido, procesa este bloque, luego lo define. Y finaliza con #endif
 void menu();
 void Leer(char cadena[]);
 float LeerFloat();
@@ -5,3 +7,4 @@ void calcularInventario(float precio[], int numobjetos);
 void ProductosCaroBarato(float precio[], int cont, char nombres [][20]);
 void encontrarProducto(char nombre[][20], int cont, float precio[]);
 void precioPromedioP(float precios[], int contador);
+#endif
